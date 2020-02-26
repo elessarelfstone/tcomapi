5082c3469e670fb1a47fa78a18ad3ef913d6bd7d
elessarelfstone <elessarelfstone@mail.ru>
2020-02-26 19:09:08 +0500

    bump to 0.1.25

--- kgd/__main__.py
+++ kgd/__main__.py
@@ -9,27 +9,9 @@ import kgd.constants as cnst
 
 from kgd.cli import parse_args
 from kgd.common import ParseFilesManager
-from kgd.constants import SERVER_IS_DOWN
+from kgd.constants import SERVER_IS_DOWN, PROLOGUE, STATUS_EXPLANATION
 from kgd.api import KgdTaxPaymentParser
 from kgd.utils import is_server_up
-
-
-def print_loading():
-    # TODO One print function
-    print('There could be parsed BINs.')
-    print('It may take some time. Building list ...')
-
-
-def print_status_map():
-    # TODO One print function and read text from file
-    print('> rqe - count of BINs we got KGD API error with. Those BINs are not supposed to be reprocessed.')
-    print('> rse - count of occurrences when we got bad response(KGD requests count limitation, ')
-    print('html(from proxy e.g. squid) or some other trash not xml formatted). Those BINs are supposed to be reprocessed.')
-    print('> se - count of occurrences when we failed(connection, network, 500, etc ). ')
-    print(' Those BINs are supposed to be reprocessed. ')
-    print('> s - count of BINs we successfully processed')
-    print('> R - indicates reprocessing')
-    print('>> All these indicators are actual for only this launch!')
 
 
 def main():
@@ -38,26 +20,25 @@ def main():
 
     if not is_server_up(KgdTaxPaymentParser.host):
         print(SERVER_IS_DOWN)
-        sys.exit()
+        sys.exit(-1)
 
     fm = ParseFilesManager(p.fpath)
     pr = KgdTaxPaymentParser(p.token, p.timeout)
 
-    print_loading()
+    print(PROLOGUE)
     bins = deque(fm.load_ids())
 
-    print_status_map()
+    # print_status_map()
+    print(STATUS_EXPLANATION)
     with tqdm(total=fm.all_count) as pbar:
         pbar.update(fm.prs_count)
 
         while bins:
-
             incr = 1
             r = False
-
             if pr.failed:
-                # inform tqdm about reprocessing
                 _bin = pr.failed.popleft()
+                # inform tqdm about reprocessing
                 incr = 0
                 r = True
             else:
@@ -71,7 +52,7 @@ def main():
                 r = pr.process_id(_bin, p.date_range, fm.curr_file, fm.parsed_file)
                 if r < 0:
                     print(SERVER_IS_DOWN)
-                    exit(-1)
+                    exit()
 
             except KeyboardInterrupt:
                 sys.exit(1)
--- kgd/constants.py
+++ kgd/constants.py
@@ -1,26 +1,18 @@
-from enum import IntEnum
 
-HOST = "data.egov.kz"
-
-
-class ExitStatus(IntEnum):
-    """Program exit code constants."""
-    SUCCESS = 0
-    ERROR = 1
-    ERROR_NO_BINS = 2
-    ERROR_TOO_MANY_FAILS = 3
-    ERROR_SERVER_IS_DOWN = 4
-
-    # 128+2 SIGINT <http://www.tldp.org/LDP/abs/html/exitcodes.html>
-    ERROR_CTRL_C = 130
-
-
-PROCESSED_NOT_EXISTS_MESSAGE = 'There are no processed BINs.'+'\n' +\
-                               'Loading BINs to start retrieving data ... '
-PROCESSED_EXISTS_MESSAGE = 'There are processed BINs.'+'\n' +\
-                           'Loading BINs to continue retrieving data ...'
-
-NO_BINS = 'No BINs to parse'
-TOO_MANY_FAILS = 'Too many fails. Exiting...'
-SRV_IS_DOWN = 'Address is unreachable. Exiting...'
 SERVER_IS_DOWN = 'Address is unreachable. Exiting...'
+
+
+PROLOGUE = """
+There could be parsed BINs.
+It may take some time. Building list ..."""
+
+STATUS_EXPLANATION = """
+> rqe - count of BINs we got KGD API error with. Those BINs are not supposed to be reprocessed.
+> rse - count of occurrences when we got bad response(KGD requests count limitation, 
+html(from proxy e.g. squid) or some other trash not xml formatted). Those BINs are supposed to be reprocessed.
+> se - count of occurrences when we failed(connection, network, 500, etc ).
+Those BINs are supposed to be reprocessed. 
+> s - count of BINs we successfully processed
+> R - indicates reprocessing
+>> All these indicators are actual for only this launch! 
+"""
